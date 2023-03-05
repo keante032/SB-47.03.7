938e92604615d10f0fdc8164d849af85525e38bb
Kevin Templeton <templeton.kevin@gmail.com>
2023-03-04 21:15:59 -0600

    count-zeroes done and passes test

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,21 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+    let idxFirstZero = findFirstZero(arr)
+    if (idxFirstZero === -1) return 0;
+
+    return arr.length - idxFirstZero
+}
+
+function findFirstZero(arr, idxLow = 0, idxHigh = arr.length - 1) {
+    if (idxHigh >= idxLow) {
+        let idxMid = idxLow + Math.floor((idxHigh - idxLow) / 2)
+        if ((idxMid === 0 || arr[idxMid - 1] === 1) && arr[idxMid] === 0) {
+            return idxMid;
+        } else if (arr[idxMid] === 1) {
+            return findFirstZero(arr, idxMid + 1, idxHigh)
+        }
+        return findFirstZero(arr, idxLow, idxMid - 1)
+    }
+    return -1;
 }
 
 module.exports = countZeroes
